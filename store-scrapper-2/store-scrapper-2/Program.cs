707cc7768027316fa4a008b442a1a1618e544ed8
Camilo Sanchez <camilin87@gmail.com>
2017-11-07 19:25:22 -0500

    create an empty project

---
+++ store-scrapper-2/store-scrapper-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace store_scrapper_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
