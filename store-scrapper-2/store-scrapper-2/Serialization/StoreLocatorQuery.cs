2128d0bf9e2246db38308170799e9cc81f3538f7
Camilo Sanchez <camilin87@gmail.com>
2017-11-08 22:28:21 -0500

    start building up the store locator query serialization

---
+++ store-scrapper-2/store-scrapper-2/Serialization/StoreLocatorQuery.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace store_scrapper_2.Serialization
+{
+  [DataContract]
+  public class StoreLocatorQuery
+  {
+    [DataMember(Order = 0)]
+    public string InputText { get; set; }
+    [DataMember(Order = 1)]
+    public string[] Filters { get; set; } = { };
+    [DataMember(Order = 2)]
+    public int LocationType { get; set; }
+
+
+    public override string ToString()
+    {
+      using (var stream = new MemoryStream())
+      {
+        var ser = new DataContractJsonSerializer(typeof(StoreLocatorQuery));
+        ser.WriteObject(stream, this);
+        stream.Position = 0;
+
+        using (var streamReader = new StreamReader(stream))
+        {          
+          return streamReader.ReadToEnd();
+        }
+      }
+    }
+  }
+}
---
+++ store-scrapper-2/store-screapper-2-Tests/Serialization/StoreLocatorQueryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using store_scrapper_2.Serialization;
+using Xunit;
+
+namespace store_screapper_2_Tests.Serialization
+{
+  public class StoreLocatorQueryTest
+  {
+    [Fact]
+    public void CanBeConvertedToJson()
+    {
+      var expected = @"{
+        ""InputText"": ""AAAA"",
+        ""Filters"": [],
+        ""LocationType"": 3
+      }".Split()
+        .Select(_ => _.Trim())
+        .Aggregate((a, b) => a + b);  
+
+      var actual = new StoreLocatorQuery
+      {
+        InputText = "AAAA",
+        LocationType = 3
+      }.ToString();
+      
+      Assert.Equal(expected, actual);
+    }
+  }
+}
