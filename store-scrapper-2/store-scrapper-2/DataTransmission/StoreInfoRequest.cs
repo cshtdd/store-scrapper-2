818a3997a17d1834ed657ce9b0a5a21e14a50100
Camilo Sanchez <camilin87@gmail.com>
2017-11-09 09:09:13 -0500

    finish up the request building

--- store-scrapper-2/store-scrapper-2/DataTransmission/StoreInfoRequest.cs
+++ store-scrapper-2/store-scrapper-2/DataTransmission/StoreInfoRequest.cs
@@ -1,9 +1,23 @@
-﻿namespace store_scrapper_2.DataTransmission
+﻿using System.Net;
+using store_scrapper_2.Serialization;
+
+namespace store_scrapper_2.DataTransmission
 {
   public struct StoreInfoRequest
   {
     public string StoreNumber { get; }
 
     public StoreInfoRequest(string storeNumber) => StoreNumber = storeNumber;
+
+    public override string ToString()
+    {
+      var q = new StoreLocatorQuery($"#{StoreNumber}", 4, "17", "SUBWAY_PROD");
+      var qJson = q.ToJson();
+      var qUrl = WebUtility.UrlEncode(qJson);
+
+      var endpoint = "https://locator-svc.subway.com/v2//GetLocations.ashx";
+
+      return $"{endpoint}?&q={qUrl}";
+    }
   }
 }
