25aa5a928614dec7b5cfb656b515e43fc159f231
Camilo Sanchez <camilin87@gmail.com>
2017-11-10 20:58:35 -0500

    finish the data structure for StoreInfoResponse

---
+++ store-scrapper-2/store-scrapper-2/DataTransmission/StoreInfoResponse.cs
@@ -0,0 +1,31 @@
+﻿namespace store_scrapper_2.DataTransmission
+{
+  public struct StoreInfoResponse
+  {
+    public string StoreNumber { get; }
+    public string SatelliteNumber { get; }
+    
+    public string FullStoreNumber => $"{StoreNumber}-{SatelliteNumber}";
+
+    public bool IsRestricted { get; }
+
+    public string Address1 { get; }
+    public string Address2 { get; }
+    public string Address3 { get; }
+    public string City { get; }
+    public string State { get; }
+    public string PostalCode { get; }
+    public string CountryCode { get; }
+    public string CountryCode3 { get; }
+   
+    public double Latitude { get; }
+    public double Longitude { get; }
+    public string TimeZoneId { get; }
+    public int CurrentUtcOffset { get; }
+
+    public int ListingNumber { get; }
+    public string OrderingUrl { get; }
+    public string CateringUrl { get; }
+  }
+}
+
