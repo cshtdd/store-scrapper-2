854ad247c5b7f978cb06422656c1ff222e8f19c4
Camilo Sanchez <camilin87@gmail.com>
2017-11-12 12:25:57 -0500

    start defining the StoreInfoDownloader

---
+++ store-scrapper-2/store-scrapper-2/DataTransmission/StoreInfoDownloader.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace store_scrapper_2.DataTransmission
+{
+  public class StoreInfoDownloader
+  {
+    private readonly IUrlDownloader _urlDownloader;
+
+    public StoreInfoDownloader(IUrlDownloader urlDownloader)
+    {
+      _urlDownloader = urlDownloader;
+    }
+
+    public async Task<StoreInfoResponse> Download(StoreInfoRequest request)
+    {
+      return await new Task<StoreInfoResponse>(() => new StoreInfoResponse());
+    }
+  }
+}
---
+++ store-scrapper-2/store-screapper-2-Tests/DataTransmission/StoreInfoDownloaderTest.cs
@@ -0,0 +1,58 @@
+﻿using store;
+using store_scrapper_2.DataTransmission.Serialization;
+using Xunit;
+
+namespace store_screapper_2_Tests.DataTransmission
+{
+  public class StoreInfoDownloaderTest
+  {
+    [Fact]
+    public void DownloadsTheStoreInfoFromTheStoreLocator()
+    {
+      var seededResponse = GenerateStoreLocatorResponse();
+      
+    }
+
+    private static string GenerateStoreLocatorResponse()
+    {
+      var seededStoreLocatorResponse = new StoresLocatorResponse
+      {
+        ResultData = new[]
+        {
+          new StoreInfoData
+          {
+            LocationId = new LocationId
+            {
+              StoreNumber = 67789,
+              SatelliteNumber = 4,
+              IsRestricted = true
+            },
+            Address = new Address
+            {
+              Address1 = "addr1",
+              Address2 = "add2",
+              Address3 = "addr3",
+              City = "city1",
+              CountryCode = "us",
+              CountryCode3 = "us3",
+              PostalCode = "12345",
+              StateProvCode = "ny"
+            },
+            Geo = new Geo
+            {
+              CurrentUtcOffset = 5,
+              Latitude = 34,
+              Longitude = 67,
+              TimeZoneId = "GMT"
+            },
+            ListingNumber = 12,
+            CateringUrl = "the catering",
+            OrderingUrl = "the ordering"
+          }
+        }
+      };
+      var seededResponse = $"({seededStoreLocatorResponse.ToJson()})";
+      return seededResponse;
+    }
+  }
+}
