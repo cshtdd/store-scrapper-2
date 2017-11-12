8f8eb1574fce8f5fa96ca35f3e53bbb11286327d
Camilo Sanchez <camilin87@gmail.com>
2017-11-12 00:26:56 -0500

    create a deserializer method

--- store-scrapper-2/store-scrapper-2/DataTransmission/Serialization/GenericJsonSerializer.cs
+++ store-scrapper-2/store-scrapper-2/DataTransmission/Serialization/GenericJsonSerializer.cs
@@ -9,14 +9,28 @@ namespace store_scrapper_2.DataTransmission.Serialization
     {
       using (var stream = new MemoryStream())
       {
-        var ser = new DataContractJsonSerializer(sender.GetType());
-        ser.WriteObject(stream, sender);
+        var serializer = new DataContractJsonSerializer(sender.GetType());
+        serializer.WriteObject(stream, sender);
         stream.Position = 0;
 
         using (var streamReader = new StreamReader(stream))
         {          
           return streamReader.ReadToEnd();
         }
+      }
+    }
+
+    public static T FromJson<T>(string json)
+    {
+      using (var stream = new MemoryStream())
+      using (var streamWriter = new StreamWriter(stream))
+      {
+        streamWriter.Write(json);
+        streamWriter.Flush();
+        stream.Position = 0;
+        
+        var serializer = new DataContractJsonSerializer(typeof(T));
+        return (T) serializer.ReadObject(stream);
       }
     }
   }
