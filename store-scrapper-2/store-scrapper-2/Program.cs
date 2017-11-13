7370d144093d44392998ea5599d5d328b3ee5ac9
Camilo Sanchez <camilin87@gmail.com>
2017-11-12 21:48:47 -0500

    minor program refactoring

--- store-scrapper-2/store-scrapper-2/Program.cs
+++ store-scrapper-2/store-scrapper-2/Program.cs
@@ -10,17 +10,27 @@ namespace store_scrapper_2
     // ReSharper disable once UnusedParameter.Local
     public static async Task Main(string[] args)
     {
-      var downloader = new StoreInfoDownloader(new UrlDownloader());
-
       Console.WriteLine($"Launching Program with args={string.Join(",", args)}");
-
-      var request = new StoreInfoRequest("11111", "0");
       
+      var response = await DownloadStore();
+      Console.WriteLine($"response={response}");
+
+      Console.WriteLine(new string('=', 16));
+
+      Console.WriteLine("AAAAA");
+      
+      Console.WriteLine(new string('=', 16));
+    }
+
+    private static async Task<StoreInfoResponse> DownloadStore()
+    {
+      var request = new StoreInfoRequest("11111", "0");
+
       Console.WriteLine($"Sending Request: {request}");
 
-      var response = await downloader.DownloadAsync(request);
-
-      Console.WriteLine(response);
+      return await CreateDownloader().DownloadAsync(request);
     }
+
+    private static StoreInfoDownloader CreateDownloader() => new StoreInfoDownloader(new UrlDownloader());
   }
 }
