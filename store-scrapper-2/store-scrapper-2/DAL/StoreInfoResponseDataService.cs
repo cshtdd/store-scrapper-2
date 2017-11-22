62362741457c84aa9e52c3211c8ae230a8aa6fbf
Camilo Sanchez <camilin87@gmail.com>
2017-11-21 19:05:56 -0500

    remove dataservice duplication

--- store-scrapper-2/store-scrapper-2/DAL/StoreInfoResponseDataService.cs
+++ store-scrapper-2/store-scrapper-2/DAL/StoreInfoResponseDataService.cs
@@ -12,53 +12,56 @@ namespace store
   {
     public async Task<bool> ContainsStoreAsync(string storeNumber, string satellite)
     {
-      using (var db = new StoreDataContext())
+      using (var db = CreateContext())
       {
         return await db.Stores
           .Where(_ => _.StoreNumber == storeNumber && _.SatelliteNumber == satellite)
           .AnyAsync();
-      }      
+      }
     }
 
     public async Task CreateNewAsync(StoreInfoResponse response)
     {
       var storeInfo = Mapper.Map<Store>(response);
       
-      using (var db = new StoreDataContext())
+      using (var db = CreateContext())
       {
         db.Stores.Add(storeInfo);
         
-        var changedEntries = await db.SaveChangesAsync();
-
-        Console.WriteLine($"changedEntries={changedEntries}");
-
-        if (changedEntries == 0)
-        {
-          throw new InvalidOperationException();
-        }
+        await SaveContextChanges(db);
       }
     }
 
     public async Task UpdateAsync(StoreInfoResponse response)
     {
-      using (var db = new StoreDataContext())
+      using (var db = CreateContext())
       {
         var storeInfo = await db.Stores
           .Where(_ => _.StoreNumber == response.StoreNumber && _.SatelliteNumber == response.SatelliteNumber)
           .FirstAsync();
 
-        var updatedStoreInfo = Mapper.Map<StoreInfoResponse, Store>(response, storeInfo);
+        var updatedStoreInfo = Mapper.Map(response, storeInfo);
 
         db.Entry(updatedStoreInfo).State = EntityState.Modified;
         
-        var changedEntries = await db.SaveChangesAsync();
+        await SaveContextChanges(db);
+      }
+    }
+    
+    private static StoreDataContext CreateContext()
+    {
+      return new StoreDataContext();
+    }
 
-        Console.WriteLine($"changedEntries={changedEntries}");
+    private static async Task SaveContextChanges(StoreDataContext db)
+    {
+      var changedEntries = await db.SaveChangesAsync();
 
-        if (changedEntries == 0)
-        {
-          throw new InvalidOperationException();
-        }
+      Console.WriteLine($"changedEntries={changedEntries}");
+
+      if (changedEntries == 0)
+      {
+        throw new InvalidOperationException();
       }
     }
   }
