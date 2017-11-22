2e93d36b003ac3e462728a000a4b7e6ee52b4db3
Camilo Sanchez <camilin87@gmail.com>
2017-11-21 19:36:48 -0500

    start working on the SingleStoreProcessor

---
+++ store-scrapper-2/store-scrapper-2/SingleStoreProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using store;
+using store_scrapper_2.DataTransmission;
+
+namespace store_scrapper_2
+{
+  public class SingleStoreProcessor
+  {
+    private readonly IStoreInfoDownloader _downloader;
+    private readonly IStoreInfoResponseDataService _dataService; 
+
+    public SingleStoreProcessor(IStoreInfoDownloader downloader, IStoreInfoResponseDataService dataService)
+    {
+      _downloader = downloader;
+      _dataService = dataService;
+    }
+
+    public async Task Process(string storeNumber, string satelliteNumber)
+    {
+      await _downloader.DownloadAsync(new StoreInfoRequest(storeNumber, satelliteNumber));
+    }
+  }
+}
---
+++ store-scrapper-2/store-screapper-2-Tests/SingleStoreProcessorTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using NSubstitute;
+using store_scrapper_2;
+using store_scrapper_2.DataTransmission;
+using Xunit;
+
+namespace store_screapper_2_Tests
+{
+  public class SingleStoreProcessorTest
+  {
+    [Fact]
+    public async void DownloadsTheStoreData()
+    {
+      var downloader = Substitute.For<IStoreInfoDownloader>();
+      downloader.DownloadAsync(Arg.Is<StoreInfoRequest>(_ => _.FullStoreNumber == "55555-3"))
+        .Returns(Task.FromResult(new StoreInfoResponse()));
+      
+      var processor = new SingleStoreProcessor(downloader, null);
+
+      await processor.Process("55555", "3");
+
+      downloader
+        .Received(1)
+        .DownloadAsync(Arg.Is<StoreInfoRequest>(_ => _.FullStoreNumber == "55555-3"));
+    }
+  }
+}
