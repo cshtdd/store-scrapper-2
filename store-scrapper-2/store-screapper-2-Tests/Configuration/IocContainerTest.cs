c0f91b481dad93095330ec0ecc3dca328e5f850d
Camilo Sanchez <camilin87@gmail.com>
2017-11-30 08:19:45 -0500

    test the ioccontainer

---
+++ store-scrapper-2/store-screapper-2-Tests/Configuration/IocContainerTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using store_scrapper_2;
+using store_scrapper_2.Configuration;
+using Xunit;
+
+namespace store_screapper_2_Tests.Configuration
+{
+  public class IocContainerTest
+  {
+    public IocContainerTest()
+    {
+      IocContainer.Initialize();     
+    }
+    
+    [Fact]
+    public void CanResolve()
+    {
+      IocContainer.Resolve<SingleStoreProcessor>().Should().NotBeNull();
+    }
+  }
+}
