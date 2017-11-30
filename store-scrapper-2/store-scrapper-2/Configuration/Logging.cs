02162671b3b916dae4fda6cf4b33aeeb72e2e2ac
Camilo Sanchez <camilin87@gmail.com>
2017-11-30 08:24:45 -0500

    extract the logging configuration to its own file

---
+++ store-scrapper-2/store-scrapper-2/Configuration/Logging.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Xml;
+using log4net;
+using log4net.Config;
+
+namespace store_scrapper_2.Configuration
+{
+  public static class Logging
+  {
+    public static void Initialize()
+    {
+      var log4NetConfig = new XmlDocument();
+      log4NetConfig.Load(File.OpenRead("log4net.config"));
+
+      var repo = LogManager.CreateRepository(
+        Assembly.GetEntryAssembly(),
+        typeof(log4net.Repository.Hierarchy.Hierarchy));
+
+      XmlConfigurator.Configure(repo, log4NetConfig["log4net"]);
+    }
+  }
+}
--- store-scrapper-2/store-scrapper-2/Program.cs
+++ store-scrapper-2/store-scrapper-2/Program.cs
@@ -1,9 +1,6 @@
-﻿using System.IO;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Threading.Tasks;
-using System.Xml;
 using log4net;
-using log4net.Config;
 using store_scrapper_2.Configuration;
 
 namespace store_scrapper_2
@@ -15,7 +12,7 @@ namespace store_scrapper_2
     // ReSharper disable once UnusedParameter.Local
     public static async Task Main(string[] args)
     {
-      ConfigureLogging();
+      Logging.Initialize();
       
       Logger.Info($"Launching Program with args={string.Join(",", args)}");
 
@@ -23,18 +20,6 @@ namespace store_scrapper_2
       await CreateSingleStoreProcessor().Process(args[0]);
       
       Logger.Info("Ending program");
-    }
-
-    private static void ConfigureLogging()
-    {
-      var log4NetConfig = new XmlDocument();
-      log4NetConfig.Load(File.OpenRead("log4net.config"));
-
-      var repo = LogManager.CreateRepository(
-        Assembly.GetEntryAssembly(),
-        typeof(log4net.Repository.Hierarchy.Hierarchy));
-
-      XmlConfigurator.Configure(repo, log4NetConfig["log4net"]);
     }
 
     private static SingleStoreProcessor CreateSingleStoreProcessor() => IocContainer.Resolve<SingleStoreProcessor>();
