55e42eaabf8cc395c83f7cedfec6e9c03c249a93
Camilo Sanchez <camilin87@gmail.com>
2017-11-30 09:53:16 -0500

    start parameterizing the context initialization

--- store-scrapper-2/store-scrapper-2/DAL/Db/StoreDataContext.cs
+++ store-scrapper-2/store-scrapper-2/DAL/Db/StoreDataContext.cs
@@ -4,11 +4,18 @@ namespace store_scrapper_2.DAL
 {
   public class StoreDataContext : DbContext
   {
+    private readonly string _databaseName;
+
+    public StoreDataContext(string databaseName="stores.db")
+    {
+      _databaseName = databaseName;
+    }
+
     public DbSet<Store> Stores { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-      optionsBuilder.UseSqlite("Data Source=stores.db");
+      optionsBuilder.UseSqlite($"Data Source={_databaseName}");
     }
   }
 }
