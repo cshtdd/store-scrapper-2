65161942045a217a55cf17857251d2a3c982da84
Camilo Sanchez <camilin87@gmail.com>
2017-11-30 09:06:44 -0500

    refactor the logging configuration to be more extensible

--- store-scrapper-2/store-scrapper-2/Configuration/Logging.cs
+++ store-scrapper-2/store-scrapper-2/Configuration/Logging.cs
@@ -2,22 +2,102 @@
 using System.Reflection;
 using System.Xml;
 using log4net;
+using log4net.Appender;
 using log4net.Config;
+using log4net.Core;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
 
 namespace store_scrapper_2.Configuration
 {
   public static class Logging
   {
-    public static void Initialize()
+    public enum ConfigurationSource
     {
-      var log4NetConfig = new XmlDocument();
-      log4NetConfig.Load(File.OpenRead("log4net.config"));
-
-      var repo = LogManager.CreateRepository(
-        Assembly.GetEntryAssembly(),
-        typeof(log4net.Repository.Hierarchy.Hierarchy));
-
-      XmlConfigurator.Configure(repo, log4NetConfig["log4net"]);
+      Memory,
+      File
     }
+    
+    public static void Initialize(ConfigurationSource source = ConfigurationSource.Memory)
+    {
+      if (source == ConfigurationSource.File)
+      {
+        FileConfiguration.Load("log4net.config");
+        return;
+      }
+      
+      MemoryConfiguration.Load();
+    }
+    
+    private static class FileConfiguration
+    {     
+      public static void Load(string configFilename)
+      {
+        var log4NetConfig = new XmlDocument();
+        log4NetConfig.Load(File.OpenRead(configFilename));
+
+        var repo = LogManager.CreateRepository(
+          Assembly.GetEntryAssembly(),
+          typeof(Hierarchy));
+
+        XmlConfigurator.Configure(repo, log4NetConfig["log4net"]);
+      }
+    }
+
+    private static class MemoryConfiguration
+    {
+      private static string GetLogFilename() => $"logs/{Assembly.GetEntryAssembly().GetName().Name}.log";
+      private static string GetLogFormat() => "%-5p %utcdate{yyyy-MM-dd HH:mm:ss} %message%newline";
+
+      public static void Load()
+      {
+        var hierarchy = (Hierarchy)LogManager.GetRepository(Assembly.GetEntryAssembly());
+
+        var appenders = new[]
+        {
+          GetRollingFileAppender(),
+          GetConsoleAppender()
+        };
+      
+        foreach (var appender in appenders)
+        {
+          (appender as IOptionHandler)?.ActivateOptions();
+          hierarchy.Root.AddAppender(appender);       
+        }
+      
+        hierarchy.Root.Level = Level.All;
+        hierarchy.Configured = true;
+      }
+
+      private static IAppender GetConsoleAppender() => new ConsoleAppender
+      {
+        Layout = GetLayout()
+      };
+
+      private static IAppender GetRollingFileAppender() => new RollingFileAppender
+      {
+        LockingModel = GetLockingModel(),
+        File = GetLogFilename(),
+        StaticLogFileName = true,
+        AppendToFile = true,
+        RollingStyle = RollingFileAppender.RollingMode.Composite,
+        MaxSizeRollBackups = 10,
+        MaximumFileSize = "5MB",
+        Layout = GetLayout()
+      };
+
+      private static FileAppender.MinimalLock GetLockingModel() => new FileAppender.MinimalLock();
+
+      private static PatternLayout GetLayout()
+      {
+        var patternLayout = new PatternLayout
+        {
+          ConversionPattern = GetLogFormat()
+        };
+        patternLayout.ActivateOptions();
+        return patternLayout;
+      }
+
+    }    
   }
 }
