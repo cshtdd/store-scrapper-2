d7f60fac02346b9a79e601550904d90992a8aa69
Camilo Sanchez <camilin87@gmail.com>
2017-12-01 22:32:39 -0500

    tests the migrations

---
+++ store-scrapper-2/store-scrapper-2-int-Tests/DAL/PersistenceInitializerTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using store_scrapper_2;
+using store_scrapper_2_int_Tests.Utils;
+using Xunit;
+
+namespace store_scrapper_2_int_Tests.DAL
+{
+  public class PersistenceInitializerTest : BaseDatabaseTest
+  {
+    [Fact]
+    public async Task RunsTheMigrations()
+    {
+      ContextFactory.Create().Database.GetPendingMigrations().Should().NotBeEmpty();
+
+      DatabaseExists.Should().BeFalse();
+      
+      await new PersistenceInitializer(ContextFactory).Initialize();
+
+      ContextFactory.Create().Database.GetPendingMigrations().Should().BeEmpty();
+      
+      DatabaseExists.Should().BeTrue();
+    }
+  }
+}
