c4dcf9a271df5a2104a61197bf28112a39d69cf4
Camilo Sanchez <camilin87@gmail.com>
2017-12-02 00:11:48 -0500

    the tests should use different databases in order to run in parallel

--- store-scrapper-2/store-scrapper-2-int-Tests/Utils/BaseDatabaseTest.cs
+++ store-scrapper-2/store-scrapper-2-int-Tests/Utils/BaseDatabaseTest.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.IO;
+using store_scrapper_2.Configuration;
 using store_scrapper_2.DAL;
 
 namespace store_scrapper_2_int_Tests.Utils
 {
   public abstract class BaseDatabaseTest : IDisposable
   {
-    private const string TestDatabaseName = "test_stores.db";
+    private readonly string TestDatabaseName = $"test_stores{Guid.NewGuid():X}.db";
     
-    protected readonly IStoreDataContextFactory ContextFactory = new StoreDataContextFactory(TestDatabaseName);
+    protected IStoreDataContextFactory ContextFactory => new StoreDataContextFactory(TestDatabaseName);
     
     protected bool DatabaseExists => File.Exists(TestDatabaseName);
+
+    static BaseDatabaseTest() => Mappings.Configure();
     
     public void Dispose()
     {
