539b20092439bc3e09c298f0810998caa7a21499
Camilo Sanchez <camilin87@gmail.com>
2017-12-01 22:32:33 -0500

    start building infrastructure for the db integration tests

---
+++ store-scrapper-2/store-scrapper-2-int-Tests/DAL/TestDatabaseInfrastructureTest.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using store_scrapper_2_int_Tests.Utils;
+using Xunit;
+
+namespace store_scrapper_2_int_Tests.DAL
+{
+  public class TestDatabaseInfrastructureTest : BaseDatabaseTest
+  {
+    [Fact]
+    public void TestDatabaseDoesNotExist()
+    {
+      DatabaseExists.Should().BeFalse();
+    }
+  }
+}
---
+++ store-scrapper-2/store-scrapper-2-int-Tests/Utils/BaseDatabaseTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using store_scrapper_2.DAL;
+
+namespace store_scrapper_2_int_Tests.Utils
+{
+  public abstract class BaseDatabaseTest : IDisposable
+  {
+    private const string TestDatabaseName = "test_stores.db";
+    
+    protected readonly IStoreDataContextFactory ContextFactory = new StoreDataContextFactory(TestDatabaseName);
+    
+    protected bool DatabaseExists => File.Exists(TestDatabaseName);
+    
+    public void Dispose()
+    {
+      if (File.Exists(TestDatabaseName))
+      {
+        File.Delete(TestDatabaseName);
+      }
+    }
+  }
+}
