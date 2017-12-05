d73a255d1045a12c02665f35cdc16698d528e3ac
Camilo Sanchez <camilin87@gmail.com>
2017-12-05 07:57:27 -0500

    test the store update

--- store-scrapper-2/store-scrapper-2-int-Tests/DAL/StoreInfoResponseDataServiceTest.cs
+++ store-scrapper-2/store-scrapper-2-int-Tests/DAL/StoreInfoResponseDataServiceTest.cs
@@ -11,7 +11,14 @@ using Xunit;
 namespace store_scrapper_2_int_Tests.DAL
 {
   public class StoreInfoResponseDataServiceTest : DatabaseTest
-  {   
+  {
+    private readonly StoreInfoResponseDataService dataService;
+
+    public StoreInfoResponseDataServiceTest()
+    {
+      dataService = new StoreInfoResponseDataService(ContextFactory);
+    }
+    
     [Fact]
     public async Task SavesANewResponse()
     {
@@ -28,8 +35,6 @@ namespace store_scrapper_2_int_Tests.DAL
       {
         context.Stores.Should().BeEmpty();
       }
-
-      var dataService = new StoreInfoResponseDataService(ContextFactory);
       
       responses.ForEach(async _ => await dataService.CreateNewAsync(_));
       
@@ -43,6 +48,37 @@ namespace store_scrapper_2_int_Tests.DAL
       responses.ForEach(async _ => (await dataService.ContainsStoreAsync(_.StoreNumber, _.SatelliteNumber)).Should().BeTrue());
 
       (await dataService.ContainsStoreAsync("7777", "3")).Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task UpdatesAnExistingResponse()
+    {
+      await new PersistenceInitializer(ContextFactory).InitializeAsync();
+
+      var originalResponse = StoreInfoResponseFactory.Create("11111-3");
+
+      await dataService.CreateNewAsync(originalResponse);
+      (await dataService.ContainsStoreAsync(originalResponse.StoreNumber, originalResponse.SatelliteNumber)).Should().BeTrue();
+      
+      var updatedResponse = CreateUpdatedResponse(originalResponse);
+
+      await dataService.UpdateAsync(updatedResponse);
+      (await dataService.ContainsStoreAsync(originalResponse.StoreNumber, originalResponse.SatelliteNumber)).Should().BeTrue();
+      
+      using (var context = ContextFactory.Create())
+      {
+        context.Stores.Count().Should().Be(1);
+
+        await context.ShouldContainStoreEquivalentTo(updatedResponse);
+      }
+    }
+
+    private static StoreInfoResponse CreateUpdatedResponse(StoreInfoResponse originalResponse)
+    {
+      var updatedResponse = StoreInfoResponseFactory.Create("00000-0");
+      updatedResponse.StoreNumber = originalResponse.StoreNumber;
+      updatedResponse.SatelliteNumber = originalResponse.SatelliteNumber;
+      return updatedResponse;
     }
   }
 }
