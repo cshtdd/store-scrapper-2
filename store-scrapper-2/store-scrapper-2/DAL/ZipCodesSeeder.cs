2415d88e53183a109bd04ed47b27de0a70c66f15
Camilo Sanchez <camilin87@gmail.com>
2017-12-12 00:02:48 -0500

    shortcircuit the zipcodes seed if not needed

--- store-scrapper-2/store-scrapper-2/DAL/ZipCodesSeeder.cs
+++ store-scrapper-2/store-scrapper-2/DAL/ZipCodesSeeder.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using CsvHelper;
+using Microsoft.EntityFrameworkCore;
 using store_scrapper_2.Configuration;
 using store_scrapper_2.DAL;
 using store_scrapper_2.DAL.Db;
@@ -10,9 +11,10 @@ namespace store_scrapper_2
 {
   public class ZipCodesSeeder
   {
+    private static readonly log4net.ILog Logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
     private readonly IStoreDataContextFactory _contextFactory;
     private readonly IConfigurationReader _configurationReader;
-    private static readonly log4net.ILog Logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
     public ZipCodesSeeder(IStoreDataContextFactory contextFactory, IConfigurationReader configurationReader)
     {
@@ -22,17 +24,34 @@ namespace store_scrapper_2
     
     public async Task SeedAsync()
     {
+      Logger.Debug("ZipCode Seed Started");
+
+      var shouldSeed = await ShouldSeedAsync();
+
+      if (!shouldSeed)
+      {
+        Logger.Debug("ZipCode Seed Skipped");
+        return;
+      }
+      
       await ClearZipCodesAsync();
       var seedFilename = _configurationReader.Read(ConfigurationKeys.SeedsZipsFilename);
       await LoadZipCodesFromCsvAsync(seedFilename);
+    }
+
+    private async Task<bool> ShouldSeedAsync()
+    {
+      using (var context = _contextFactory.Create())
+      {
+        var containsData = await context.Zips.AnyAsync();
+        return !containsData;
+      }
     }
 
     private async Task LoadZipCodesFromCsvAsync(string filename)
     {
       using (var context = _contextFactory.Create())
       {
-        Logger.Debug("ZipCode Seed Started");
-
         using (var streamReader = new StreamReader(filename))
         {
           var csvReader = new CsvReader(streamReader);
