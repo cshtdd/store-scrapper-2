b8491ea2a389898f079987cf97e4ca007f9feed2
Camilo Sanchez <camilin87@gmail.com>
2017-12-16 23:26:13 -0500

    tweak the speed and add some logging to the exception

--- store-scrapper-2/store-scrapper-2/DataTransmission/UrlDownloader.cs
+++ store-scrapper-2/store-scrapper-2/DataTransmission/UrlDownloader.cs
@@ -6,17 +6,29 @@ namespace store_scrapper_2.DataTransmission
 {
   public class UrlDownloader : IUrlDownloader
   {
+    private static readonly log4net.ILog Logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
     public string Download(string url) => DownloadAsync(url).Result;
 
     public async Task<string> DownloadAsync(string url)
     {
       var request = WebRequest.CreateHttp(url);
-      
-      using (var response = request.GetResponse())
-      using (var responseStream = response.GetResponseStream())
-      using (var reader = new StreamReader(responseStream))
+
+      try
       {
-        return await reader.ReadToEndAsync();
+        Logger.Debug($"Download; {url}");
+
+        using (var response = request.GetResponse())
+        using (var responseStream = response.GetResponseStream())
+        using (var reader = new StreamReader(responseStream))
+        {
+          return await reader.ReadToEndAsync();
+        }
+      }
+      catch (WebException ex)
+      {
+        Logger.Debug("Download Error", ex);
+        throw;
       }
     }
   }
