e7aa1fabe11bdbd8730bacf6c95f346f2ee1b4e6
Camilo Sanchez <camilin87@gmail.com>
2018-01-22 13:32:00 -0500

    remove not needed configuration keys

--- store-scrapper-2/store-scrapper-2/Configuration/ConfigurationKeys.cs
+++ store-scrapper-2/store-scrapper-2/Configuration/ConfigurationKeys.cs
@@ -7,11 +7,7 @@ namespace store_scrapper_2.Configuration
     public const string ConnectionStringsStoresDb = "ConnectionStrings:StoresDb";
     public const string EfLogEnabled = "Ef:LogEnabled";
     public const string SeedsZipsFilename = "Seeds:ZipsFilename";
-    [Obsolete]
-    public const string ZipCodesBatchSize = "ZipCodes:BatchSize";
     public const string ZipCodesDelayMs = "ZipCodes:DelayMs";
-    [Obsolete]
-    public const string ZipCodesMaxBatchCount = "ZipCodes:MaxBatchCount";
     public const string ZipCodesRunContinuosly = "ZipCodes:RunContinuosly";
     public const string StoresWriteCacheExpirationMs = "Stores:WriteCacheExpirationMs";
   }
