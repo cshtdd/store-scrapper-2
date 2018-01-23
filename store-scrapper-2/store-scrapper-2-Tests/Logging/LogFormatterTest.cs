de257f686fe4163cc0a1224f1967f1e749f15535
Camilo Sanchez <camilin87@gmail.com>
2018-01-23 07:23:36 -0500

    start defining how LogFormatter is going to work

---
+++ store-scrapper-2/store-scrapper-2-Tests/Logging/LogFormatterTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using store_scrapper_2.Logging;
+using Xunit;
+
+namespace store_scrapper_2_Tests.Logging
+{
+  public class LogFormatterTest
+  {
+    [Fact]
+    public void FormatsAnEmptyObject()
+    {
+      format(null, string.Empty);
+      format(new Dictionary<string, object>(), string.Empty);
+    }
+
+    [Fact]
+    public void FormatsFieldNames()
+    {
+      format(new Dictionary<string, object>
+      {
+        {"Key1", null}
+      }, "Key1:null");
+      
+      format(new Dictionary<string, object>
+      {
+        {"Key1", null},
+        {"Key2", null}
+      }, "Key1:null, Key2:null");
+    }
+
+    private void format(IDictionary<string, object> input, string expected)
+    {
+      LogFormatter.Format(input).Should().Be(expected);      
+    }
+  }
+}
---
+++ store-scrapper-2/store-scrapper-2/Logging/LogFormatter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace store_scrapper_2.Logging
+{
+  public static class LogFormatter
+  {
+    private static readonly string NO_OUTPUT = string.Empty;
+    private static readonly string FIELD_SEPARATOR = ", ";
+
+    public static string Format(IDictionary<string, object> logEntry)
+    {
+      if (logEntry == null)
+      {
+        return NO_OUTPUT;
+      }
+
+      if (logEntry.Keys.Count == 0)
+      {
+        return NO_OUTPUT;
+      }
+
+      return logEntry
+        .Select(_ => Format(_.Key, _.Value))
+        .Aggregate((i, j) => i + FIELD_SEPARATOR + j);
+    }
+
+    private static string Format(string key, object value)
+    {
+      return $"{key}:null";     
+    }
+  }
+}
