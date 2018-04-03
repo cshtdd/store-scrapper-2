b01e58b2a5ac94a8e34fe322a53f558e2845642f
Camilo Sanchez <camilin87@gmail.com>
2018-04-03 19:58:23 -0400

    cleanup the SingleZipCodeProcessorTests

--- store-scrapper-2-Tests/Services/SingleZipCodeProcessorTest.cs
+++ store-scrapper-2-Tests/Services/SingleZipCodeProcessorTest.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using store_scrapper_2;
 using store_scrapper_2.Model;
 using store_scrapper_2.DataTransmission;
@@ -13,6 +18,19 @@ namespace store_scrapper_2_Tests.Services
 {
   public class SingleZipCodeProcessorTest
   {
+    private readonly ZipCode _zipCode = new ZipCode("55555", 17, 45);
+
+    private readonly IStoreInfoDownloader _downloader = Substitute.For<IStoreInfoDownloader>();
+    private readonly IStoresPersistor _persistor = Substitute.For<IStoresPersistor>();
+    private readonly IZipCodeDataService _zipCodeDataService = Substitute.For<IZipCodeDataService>();
+
+    private readonly SingleZipCodeProcessor _processor;
+
+    public SingleZipCodeProcessorTest()
+    {
+      _processor = new SingleZipCodeProcessor(_downloader, _persistor, _zipCodeDataService);
+    }
+    
     [Fact]
     public async Task DownloadsAndPersistsTheStoreData()
     {
@@ -20,29 +38,23 @@ namespace store_scrapper_2_Tests.Services
         .Create(10)
         .Select(StoreInfoFactory.Create)
         .ToArray();
-      
-      var downloader = Substitute.For<IStoreInfoDownloader>();
-      downloader.DownloadAsync(Arg.Any<ZipCode>())
+
+      _downloader.DownloadAsync(Arg.Any<ZipCode>())
         .Returns(Task.FromResult<IEnumerable<StoreInfo>>(stores));
+      
 
-      var persistor = Substitute.For<IStoresPersistor>();
-      var zipCodeDataService = Substitute.For<IZipCodeDataService>();
-
-
-      var zipCode = new ZipCode("55555", 17, 45);
-      await new SingleZipCodeProcessor(downloader, persistor, zipCodeDataService)
-        .ProcessAsync(zipCode);
+      await _processor.ProcessAsync(_zipCode);
 
       
-      await downloader
+      await _downloader
         .Received(1)
-        .DownloadAsync(zipCode);
+        .DownloadAsync(_zipCode);
 
-      await persistor
+      await _persistor
         .Received(1)
         .PersistAsync(Arg.Is<IEnumerable<StoreInfo>>(_ => _.SequenceEqual(stores)));
 
-      await zipCodeDataService
+      await _zipCodeDataService
         .Received(1)
         .UpdateZipCodeAsync("55555");
     }
