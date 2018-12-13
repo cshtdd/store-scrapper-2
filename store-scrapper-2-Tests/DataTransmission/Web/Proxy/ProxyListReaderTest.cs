bf8a698a87b2779dcc4070e825d8ef37a9985836
Camilo Sanchez <camilin87@gmail.com>
2018-12-12 19:10:18 -0500

    first implementation almost correct of the ProxyListReader

--- store-scrapper-2-Tests/DataTransmission/Web/Proxy/ProxyListReaderTest.cs
+++ store-scrapper-2-Tests/DataTransmission/Web/Proxy/ProxyListReaderTest.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using FluentAssertions;
 using NSubstitute;
 using store_scrapper_2.DataTransmission.Web;
 using store_scrapper_2.DataTransmission.Web.Proxy;
@@ -21,6 +23,30 @@ namespace store_scrapper_2_Tests.DataTransmission.Web.Proxy
       reader.Read();
 
       urlDownloader.Received().Download("https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list.txt");
+    }
+
+    [Fact]
+    public void CorrectlyParsesTheProxyList()
+    {
+      urlDownloader.Download(Arg.Any<string>())
+        .Returns(@"Proxy list updated at Tue, 11 Dec 18 05:55:08 +0300
+Mirrors=http://spys.me/proxy.txt https://twitter.com/spys_one https://t.me/spys_one
+IP address:Port Country-Anonymity(Noa/Anm/Hia)-SSL_support(S)-Google_passed(+)
+
+107.150.10.35:3128 US-N - 
+70.28.43.61:8080 CA-H-S + 
+179.43.81.135:80 PE-H + 
+186.15.233.218:45999 CR-H-S + 
+185.54.101.122:8080 PL-N-S + 
+24.37.245.42:46795 CA-H-S + 
+73.239.197.175:8080 US-N-S + 
+204.245.9.15:50928 GB-H-S + 
+103.194.89.161:8080 IN-H-S! + 
+87.248.171.166:44576 MD-H-S! + ");
+
+      var proxies = reader.Read().ToArray();
+
+      proxies.Length.Should().Be(10);
     }
   }
 }
--- store-scrapper-2/DataTransmission/Web/Proxy/ProxyListReader.cs
+++ store-scrapper-2/DataTransmission/Web/Proxy/ProxyListReader.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace store_scrapper_2.DataTransmission.Web.Proxy
 {
   public class ProxyListReader : IProxyListReader
   {
+    private const string ProxyListUrl = "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list.txt";
+
     private readonly IUrlDownloader _urlDownloader;
 
     public ProxyListReader(IUrlDownloader urlDownloader)
@@ -13,8 +17,18 @@ namespace store_scrapper_2.DataTransmission.Web.Proxy
 
     public IEnumerable<ProxyInfo> Read()
     {
-      _urlDownloader.Download("https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list.txt");
-      return null;
+      return _urlDownloader
+        .Download(ProxyListUrl)
+        .Split(Environment.NewLine)
+        .ToList()
+        .Skip(4)
+        .Select(ParseSingleLine)
+        .ToArray();
+    }
+
+    private ProxyInfo ParseSingleLine(string proxyDefinition)
+    {
+      return new ProxyInfo("127.0.0.0:80");
     }
   }
 }
