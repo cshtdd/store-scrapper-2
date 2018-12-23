2d6943d759a2810c112b75812b08f212f00012c3
Camilo Sanchez <camilin87@gmail.com>
2018-12-23 08:34:26 -0500

    add tests for the proxied url downloader exception handling

--- store-scrapper-2-Tests/DataTransmission/Web/Proxy/ProxiedUrlDownloaderTest.cs
+++ store-scrapper-2-Tests/DataTransmission/Web/Proxy/ProxiedUrlDownloaderTest.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Net;
 using FluentAssertions;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using store_scrapper_2.DataTransmission.Web;
 using store_scrapper_2.DataTransmission.Web.Proxy;
 using store_scrapper_2.DataTransmission.Web.Support;
@@ -30,12 +32,25 @@ namespace store_scrapper_2_Tests.DataTransmission.Web.Proxy
 
       _webRequestExecutor.Run(targetRequest)
         .Returns("tdd rocks");
-
       
       _downloader
         .Download("my url", proxy)
         .Should()
         .Be("tdd rocks");
+    }
+
+    [Fact]
+    public void BubblesUpWebExceptions()
+    {
+      _webRequestExecutor.Run(Arg.Any<HttpWebRequest>())
+        .Throws(new WebException("download error"));
+      
+      ((Action) (() =>
+        {
+          _downloader.Download("my url", new ProxyInfo("192.168.1.1:8080"));
+        }))
+        .Should()
+        .Throw<WebException>();
     }
   }
 }
