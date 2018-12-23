daa6975df616462960aa8a4c25e569dbc5e6ff5f
Camilo Sanchez <camilin87@gmail.com>
2018-12-23 08:26:00 -0500

    start standardizing the ProxiedUrlDownloader

--- store-scrapper-2-Tests/DataTransmission/Web/Proxy/ProxiedUrlDownloaderTest.cs
+++ store-scrapper-2-Tests/DataTransmission/Web/Proxy/ProxiedUrlDownloaderTest.cs
@@ -10,22 +10,29 @@ namespace store_scrapper_2_Tests.DataTransmission.Web.Proxy
 {
   public class ProxiedUrlDownloaderTest
   {
+    private readonly IWebRequestFactory _webRequestFactory = Substitute.For<IWebRequestFactory>();
+    private readonly IWebRequestExecutor _webRequestExecutor = Substitute.For<IWebRequestExecutor>();
+
+    private readonly ProxiedUrlDownloader _downloader;
+
+    public ProxiedUrlDownloaderTest()
+    {
+      _downloader = new ProxiedUrlDownloader(_webRequestExecutor, _webRequestFactory);
+    }
+    
     [Fact]
     public void DownloadsAUrl()
     {
       var proxy = new ProxyInfo("192.168.1.1:8080");
       var targetRequest = WebRequest.CreateHttp("https://tddapps.com");
-      
-      var factory = Substitute.For<IWebRequestFactory>();
-      factory.CreateHttp("my url", proxy)
+      _webRequestFactory.CreateHttp("my url", proxy)
         .Returns(targetRequest);
 
-      var requestExecutor = Substitute.For<IWebRequestExecutor>();
-      requestExecutor.Run(targetRequest)
+      _webRequestExecutor.Run(targetRequest)
         .Returns("tdd rocks");
 
       
-      new ProxiedUrlDownloader(requestExecutor, factory)
+      _downloader
         .Download("my url", proxy)
         .Should()
         .Be("tdd rocks");
