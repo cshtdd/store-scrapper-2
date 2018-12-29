3b78b5107f1deb2c611512de9e30b633a27e495e
Camilo Sanchez <camilin87@gmail.com>
2018-12-29 08:58:28 -0500

    the TimerFactory will always return something in the LastCreated property

--- store-scrapper-2/Services/Timing/Timers/TimerFactory.cs
+++ store-scrapper-2/Services/Timing/Timers/TimerFactory.cs
@@ -4,9 +4,16 @@ namespace store_scrapper_2.Services
 {
   public class TimerFactory : ITimerFactory
   {
-    public ITimer LastCreated { get; private set; }
+    public ITimer LastCreated { get; private set; } = new NoTimer();
 
     public ITimer Create(Action timerElapsedDelegate, uint intervalMs) => 
       LastCreated = new Timer(timerElapsedDelegate, intervalMs);
+
+    private class NoTimer : ITimer
+    {
+      public void Dispose() {}
+      public void Start() {}
+      public void Stop() {}
+    }
   }
 }
