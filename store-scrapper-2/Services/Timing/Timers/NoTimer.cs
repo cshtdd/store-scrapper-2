e58ecc7f65bc08597ebb8694ab3596f2a9838754
Camilo Sanchez <camilin87@gmail.com>
2018-12-29 09:35:52 -0500

    move the NoTimer to its own file

---
+++ store-scrapper-2/Services/Timing/Timers/NoTimer.cs
@@ -0,0 +1,11 @@
+namespace store_scrapper_2.Services
+{
+  internal sealed class NoTimer : ITimer
+  {
+    internal NoTimer(){}
+    
+    public void Dispose() {}
+    public void Start() {}
+    public void Stop() {}
+  }
+}
--- store-scrapper-2/Services/Timing/Timers/TimerFactory.cs
+++ store-scrapper-2/Services/Timing/Timers/TimerFactory.cs
@@ -8,12 +8,5 @@ namespace store_scrapper_2.Services
 
     public ITimer Create(Action timerElapsedDelegate, uint intervalMs) => 
       LastCreated = new Timer(timerElapsedDelegate, intervalMs);
-
-    private class NoTimer : ITimer
-    {
-      public void Dispose() {}
-      public void Start() {}
-      public void Stop() {}
-    }
   }
 }
