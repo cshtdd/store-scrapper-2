be451ff17d3b793fcd6c0b1871b914c661639158
Camilo Sanchez <camilin87@gmail.com>
2018-12-29 08:43:51 -0500

    implement the Timer with tons of borrowed code from the MemoryUsage performance counter

---
+++ store-scrapper-2/Services/Timing/Timers/Timer.cs
@@ -0,0 +1,68 @@
+using System;
+
+namespace store_scrapper_2.Services
+{
+  public class Timer : ITimer
+  {
+    private readonly Action _timerElapsedDelegate;
+    private readonly uint _intervalMs;
+    
+    private bool _disposed;
+    private System.Timers.Timer _timer;
+
+    internal Timer(Action timerElapsedDelegate, uint intervalMs)
+    {
+      _timerElapsedDelegate = timerElapsedDelegate;
+      _intervalMs = intervalMs;
+    }
+
+    private void TimerElapsedEventHandler(object sender, EventArgs e) => _timerElapsedDelegate();
+
+    public void Start()
+    {
+      if (_timer != null)
+      {
+        return;
+      }
+      
+      _timer = new System.Timers.Timer();
+      _timer.Interval = _intervalMs;
+      _timer.Elapsed += TimerElapsedEventHandler;
+      _timer.Start();
+    }
+
+    public void Stop()
+    {
+      if (_timer == null)
+      {
+        return;
+      }
+      
+      _timer.Elapsed -= TimerElapsedEventHandler;
+      _timer.Stop();
+      _timer.Dispose();
+      _timer = null;
+    }
+
+    public void Dispose()
+    {      
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+      if (_disposed)
+      {
+        return;       
+      }
+
+      if (disposing)
+      {
+        Stop();
+      }
+
+      _disposed = true;
+    }
+  }
+}
